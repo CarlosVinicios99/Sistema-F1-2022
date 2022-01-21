5cff1eaaebf46405083683e571116f688c2c7780
Carlos Vinicios <carlosvinicios@id.uff.br>
2022-01-20 22:55:57 -0300

    implementacao inicial da classe Temporada

--- SistemaF12022/Entities/Temporada.cs
+++ SistemaF12022/Entities/Temporada.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections.Generic;
 
 namespace SistemaF12022.Entities
 {
     class Temporada
     {
-        
+        private List<Equipe> _equipes = new List<Equipe>();
+        private List<Piloto> pilotos = new List<Piloto>();
+
+        public Temporada()
+        {
+            //chamar metodo da classe acessar dados para carregar os dados
+        }
     }
 }
